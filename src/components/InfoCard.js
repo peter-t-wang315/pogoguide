b67510c49d2420530c7bbadd39511f405406dced
peter-t-wang315 <wang.t.peter@gmail.com>
2024-06-19 22:24:34 -0700

    Updated regular info cards to look more like pokeballs

--- src/components/InfoCard.js
+++ src/components/InfoCard.js
@@ -1,4 +1,5 @@
-import { Box, Paper, Typography } from "@mui/material";
+import { Box, Divider, Paper, Typography } from "@mui/material";
+import RadioButtonCheckedIcon from "@mui/icons-material/RadioButtonChecked";
 import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
 import DescriptionOutlinedIcon from "@mui/icons-material/DescriptionOutlined";
 import { useRouter } from "next/navigation";
@@ -30,7 +31,7 @@ export default function InfoCard({
         flexDirection: "column",
         py: 2.5,
         px: 3.5,
-        borderRadius: "20px",
+        borderRadius: 10,
         position: "relative",
         ...(collapsibleDescription || href
           ? {
@@ -52,6 +53,7 @@ export default function InfoCard({
             ? "scale(0.925)"
             : "scale(1.025)",
         transition: "transform 0.3s ease-in-out",
+        overflow: "hidden",
       }}
       onClick={() => {
         if (href) push(href);
@@ -68,33 +70,86 @@ export default function InfoCard({
         }
       }}
     >
-      <Box sx={{ display: "flex", justifyContent: "space-between" }}>
-        <Typography
+      <Box
+        sx={{
+          mt: -3,
+          mx: -4,
+          backgroundColor: "primary.light",
+        }}
+      >
+        <Box
           sx={{
-            typography: {
-              sm: "h5",
-              xs: "h6",
-            },
-            fontWeight: {
-              xl: "normal",
-              xs: "normal",
-            },
-            pb: 2,
+            display: "flex",
+            justifyContent: "space-between",
+            px: 4,
+            pt: 3,
           }}
         >
-          {title}
-        </Typography>
-        {collapsibleDescription ? (
-          <InfoOutlinedIcon
-            sx={{ color: "primary.light", fontSize: 18, mt: 0.5 }}
+          <Typography
+            sx={{
+              typography: {
+                sm: "h5",
+                xs: "h6",
+              },
+              fontWeight: {
+                xl: "normal",
+                xs: "normal",
+              },
+            }}
+          >
+            {title}
+          </Typography>
+          {collapsibleDescription ? (
+            <InfoOutlinedIcon
+              sx={{ color: "primary.dark", fontSize: 18, mt: 0.5 }}
+            />
+          ) : href ? (
+            <DescriptionOutlinedIcon
+              sx={{ color: "primary.dark", fontSize: 18, mt: 0.5 }}
+            />
+          ) : null}
+        </Box>
+        <Divider
+          sx={{
+            "&::before, &::after": {
+              borderColor: "primary.dark",
+            },
+            mb: -2.4,
+            "&>.mui-qywfm8-MuiDivider-wrapper": {
+              p: 0,
+              mx: -0.34,
+            },
+          }}
+        >
+          <RadioButtonCheckedIcon
+            sx={{
+              color: "primary.main",
+              mt: 0.4,
+              fontSize: 30,
+            }}
           />
-        ) : href ? (
-          <DescriptionOutlinedIcon
-            sx={{ color: "primary.light", fontSize: 18, mt: 0.5 }}
-          />
-        ) : null}
+          {/* <Box
+            sx={{
+              display: "inline-flex",
+              alignItems: "center",
+              justifyContent: "center",
+              width: 26, // Adjust size as needed
+              height: 26, // Adjust size as needed
+              borderRadius: "50%",
+              backgroundColor: "white", // Set the background color to white
+              overflow: "hidden", // Ensure any overflow is hidden
+            }}
+          >
+            <RadioButtonCheckedIcon
+              sx={{
+                color: "primary.main",
+                fontSize: 30,
+              }}
+            />
+          </Box> */}
+        </Divider>
       </Box>
-      <Typography>{description}</Typography>
+      <Typography sx={{ mt: 3 }}>{description}</Typography>
       {listContent && <DoubleList listContent={listContent} />}
     </Paper>
   );
