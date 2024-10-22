467893c1968757f0f70db953b38e9954ec81d742
Tom1779 <tom.arad.2001@gmail.com>
2024-10-22 12:17:52 -0700

    switched from html to typography in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,22 +1,23 @@
 // components/Footer.js
-"use client"; // Add this line to specify it's a client component
+"use client";
 
 import { useTheme } from "@mui/material/styles";
+import { Typography } from "@mui/material";
 
 export default function Footer() {
-  const theme = useTheme(); // Access the theme
+  const theme = useTheme();
 
   return (
     <footer
       style={{
         textAlign: "center",
         padding: "1rem",
-        backgroundColor: theme.palette.foreground.dark, // Use color from theme
-        color: theme.palette.foreground.light, // Text color from theme
+        backgroundColor: theme.palette.foreground.dark,
+        color: theme.palette.foreground.light,
       }}
     >
-      <p>© 2024 PoGo Guide. All rights reserved.</p>
-      <p style={{ fontSize: "0.8rem", color: "white" }}>
+      <Typography>© 2024 PoGo Guide. All rights reserved.</Typography>
+      <Typography sx={{ fontSize: "0.8rem", color: "white" }}>
         Pokémon and Pokémon GO are copyright of The Pokémon Company, Niantic,
         Inc., and Nintendo. All trademarked images and names are property of
         their respective owners, and any such material is used on this site for
@@ -24,7 +25,7 @@ export default function Footer() {
         <br />
         PoGo Guide has no affiliation with The Pokémon Company, Niantic, Inc.,
         or Nintendo.
-      </p>
+      </Typography>
     </footer>
   );
 }
